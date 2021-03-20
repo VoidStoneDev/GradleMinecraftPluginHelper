0d6b3fc0c5f4dca25c55d26ec2a6c92f164fa3ad
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-20 09:55:50 +0100

    Added Plugin Main Class

---
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/OpenGMPH.java
@@ -0,0 +1,11 @@
+package nl.voidgroup.gradle.plugin.opengmph;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class OpenGMPH implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
