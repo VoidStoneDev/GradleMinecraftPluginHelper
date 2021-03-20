d861549824cdbd18ec97373f6daef6288f09ae1f
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-20 10:42:10 +0100

    Added MinecraftDataType

--- src/main/java/nl/voidgroup/gradle/plugin/opengmph/data/MinecraftDataType.java
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/data/MinecraftDataType.java
@@ -1,2 +1,7 @@
-package nl.voidgroup.gradle.plugin.opengmph.data;public interface MinecraftDataType {
+package nl.voidgroup.gradle.plugin.opengmph.data;
+
+public interface MinecraftDataType {
+    boolean equals(MinecraftDataType obj);
+    MinecraftDataType clone() throws CloneNotSupportedException;
+    String getString();
 }
