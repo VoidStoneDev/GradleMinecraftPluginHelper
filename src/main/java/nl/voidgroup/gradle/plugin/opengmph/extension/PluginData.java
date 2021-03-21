ab85edc201b0a3b9c20a4169aec4917a20518154
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-21 18:24:32 +0100

    Add pluginData extension

---
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/extension/PluginData.java
@@ -0,0 +1,8 @@
+package nl.voidgroup.gradle.plugin.opengmph.extension;
+
+import nl.voidgroup.gradle.plugin.opengmph.data.minecrarft.MinecraftServer;
+
+public class PluginData {
+    public MinecraftServer targetServer;
+    public boolean importRepo = true;
+}
